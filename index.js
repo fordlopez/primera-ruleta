6b7b958017a4f3d6fdb4434be18865dff3f74c58
Abner Lopez <fordlopez777@gmail.com>
2026-05-27 17:06:56 -0600

    primeras lineas

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-let jugadores = ['joscriss', 'cris', 'errosr', 'jochis']
-
+let jugadores = ['joscriss', 'chris', 'error404', 'jochis','Abner']
 let contenedorchips = document.querySelector('#contenedor-chips')
-
 let AgregarJugador =
     document.querySelector("#AgregarJugador")
-
 let ruleta =
     document.querySelector('#wheel')
+let botonGirar=document.querySelector('#spinButton')
+let inicio=0
+let result=document.querySelector('#result')
+let posicionActual=0
 
 const renderizaJugadores = () => {
 
@@ -20,17 +21,11 @@ const renderizaJugadores = () => {
     )
 
     for (let i = 0; i < jugadores.length; i++) {
-
-        // chips
         let span = document.createElement('span')
-
         span.className = "chip"
-
         span.textContent = jugadores[i]
-
         contenedorchips.appendChild(span)
 
-        // nombres dentro de ruleta
         let spanRuleta =
             document.createElement('span')
 
@@ -71,5 +66,28 @@ AgregarJugador.addEventListener(
 
     }
 )
+const girarRuleta = () => {
+    //Divido los grados (360) dentro de los paritcipantes.
+    let espacioPorParticipante = 360 / jugadores.length
+    //Luego 5 vueltas por defecto.
+    let grados = (360) * 5
+    //Luego voy a generar un numero aleatorio entre el numero de participantes
+    let numeroAleatorio = Math.floor(Math.random() * ((jugadores.length-1)  - 0 + 1)) + 0
+    console.log(numeroAleatorio)
+    let posicionDeGanador = (numeroAleatorio * espacioPorParticipante + (espacioPorParticipante/2));
+    let extra = 90 - posicionDeGanador
+    posicionActual =  extra - posicionActual
+    inicio += grados + posicionActual;
+
+    ruleta.style.setProperty('transition', 'transform 4s ease-out')
+    ruleta.style.transform = `rotate(${inicio}deg)`;
+
+    setTimeout(() => {
+        result.textContent = jugadores[(numeroAleatorio)]
+    }, 4100)
+
+}
+botonGirar.addEventListener('click', girarRuleta )
 
 renderizaJugadores()
+
